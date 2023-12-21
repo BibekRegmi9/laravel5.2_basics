28f189d55f47e2a76feb1d3c1455b1d2f97f20a5
bibek <vivekregmi83@gmail.com>
2023-12-21 14:21:44 +0545

    Note model is migrated

--- app/Card.php
+++ app/Card.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     //
-    
+    public function notes()
+    {
+        // Method: 1
+//        return $this->hasMany(Note::class); 
+
+        // Method: 2
+        return $this->hasMany('App\Note');
+    }
+
 }
